c9e00b3d3515760582d0345a76d927e5f10f3fbb
YO1231 <whee0211@gmail.com>
2025-11-05 03:52:58 +0900

    solve: C/C++ 기출 8-10번 풀이

---
+++ C,C++/prac10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+    int i, j;
+    scanf("%o#%x", &i, &j);
+    printf("%d %d", i, j);
+}
+
+// 입력 : 15#22
+
+/*
+정답: 13 34
+*/
---
+++ C,C++/prac9.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main()
+{
+
+    float a = 3.45678f;
+    double b = 0.0561214;
+    printf("[a]=%.2f / [b]=%.3e", a, b); // 빈칸
+}
+
+// 출력 : [a]=3.46 / [b]=5.612e-02
