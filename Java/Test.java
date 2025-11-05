8ddc12056056c1ed4261315d3d157e1006677709
YO1231 <whee0211@gmail.com>
2025-11-06 01:11:07 +0900

    solve: Java 기출 1-4번 풀이

---
+++ Java/Test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test {
+// prac2
+    public static void main(String args[]){
+        Scanner scan = new Scanner (System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.printf("%d", a +b);
+
+    }
+}
+
+/*
+정답: Scanner
+ */
+
